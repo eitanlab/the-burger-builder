53012f59f5bcc29a0bd7a80e0ee1072eb19258fb
Eitan Labendz <you@example.com>
2020-05-20 11:17:33 -0300

    Improved withErrorHandler with http-error.handler custom hook

--- src/hoc/withErrorHandler/withErrorHandler.jsx
+++ src/hoc/withErrorHandler/withErrorHandler.jsx
@@ -1,26 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Auxiliary/Auxiliary';
+import useHttpErrorHandler from '../../hooks/http-error-handler';
 
 const withErrorHandler = ( WrappedComponent, axios ) => {
     return props => {
-        const [error, setError] = useState(null)
-            const reqInterceptor = axios.interceptors.request.use(req => {
-                setError(null);
-                return req;
-            });
-            const resInterceptor = axios.interceptors.response.use(res => res, err => {
-                setError(err)
-            });
-
-        useEffect(() => {
-            axios.interceptors.request.eject(reqInterceptor);
-            axios.interceptors.request.eject(resInterceptor);
-        }, [reqInterceptor, resInterceptor]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        }
+        const [error, clearErrorMethod] = useHttpErrorHandler(axios);
 
         return (
             <Aux>
@@ -35,4 +20,4 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
     }
 }
 
-export default withErrorHandler
+export default withErrorHandler;
